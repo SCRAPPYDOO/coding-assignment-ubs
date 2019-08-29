8f1902398c0baa8f3d68c3e50f2e79eb053176f8
Marcin Makowski <marcin.scrappy.makowski@gmail.com>
2019-08-29 22:46:22 +0100

    calculation service entity creation strategy update

--- calculation-service/src/main/java/com/ubs/calculation/stream/CreateCalculationEvent.java
+++ calculation-service/src/main/java/com/ubs/calculation/stream/CreateCalculationEvent.java
@@ -1,11 +1,22 @@
 package com.ubs.calculation.stream;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.ToString;
 
-@Data
+@Getter
+@ToString
 public class CreateCalculationEvent {
 
     private final Long clientId;
 
     private final Long proposalId;
+
+    @JsonCreator
+    public CreateCalculationEvent(@JsonProperty("clientId") final Long clientId,
+                                  @JsonProperty("proposalId") final Long proposalId) {
+        this.clientId = clientId;
+        this.proposalId = proposalId;
+    }
 }
