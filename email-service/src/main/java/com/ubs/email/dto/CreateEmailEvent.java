1fd62c66f09565362aa52a16cdc047994c3dee68
Marcin Makowski <marcin.scrappy.makowski@gmail.com>
2019-08-30 01:39:40 +0100

    ema service object typo

--- email-service/src/main/java/com/ubs/email/dto/CreateEmailEvent.java
+++ email-service/src/main/java/com/ubs/email/dto/CreateEmailEvent.java
@@ -2,7 +2,6 @@ package com.ubs.email.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sun.xml.internal.ws.api.message.Attachment;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -14,11 +13,11 @@ public class CreateEmailEvent {
 
     private final Long clientId;
 
-    private final List<Attachment> attachmentList;
+    private final List<EmailAttachment> attachmentList;
 
     @JsonCreator
     public CreateEmailEvent(@JsonProperty("clientId") final Long clientId,
-                            @JsonProperty("attachmentList") final List<Attachment> attachmentList) {
+                            @JsonProperty("attachmentList") final List<EmailAttachment> attachmentList) {
         this.clientId = clientId;
         this.attachmentList = attachmentList;
     }
